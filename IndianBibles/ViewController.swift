c4a570645525b50d73f71f83b813182ebba01c2c
Rajeev Kalangi <50079880+KalangiRajeev@users.noreply.github.com>
2021-04-24 15:47:02 +0530

    Initial Commit

---
+++ IndianBibles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IndianBibles
+//
+//  Created by Admin on 24/04/21.
+//  Copyright © 2021 Rajeev Kalangi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
